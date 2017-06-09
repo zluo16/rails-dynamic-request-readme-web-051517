a33ee744390a110234bcaab0b7249c33cdc35ef2
zluo16 <zluo@alumni.berklee.edu>
2017-06-09 10:13:05 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get 'posts/:id', to: 'posts#show'
+
+  # resources :posts, only: :show
 end
